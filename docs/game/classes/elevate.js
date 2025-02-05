53336a55c25c19494f8bbc749c2eca483ca0edfe
pluvo070 <30020422+pluvo070@users.noreply.github.com>
2025-02-05 03:10:23 +0000

    fix bugs in elevate class

--- docs/game/classes/elevate.js
+++ docs/game/classes/elevate.js
@@ -47,23 +47,17 @@ class ElevatingWall{
     
 
     show() {
+        let coordinate = getTilePosition(this.imgIndex);
         let offsetX = (player[this.levelIndex].x - windowWidth / 2);
         let offsetY = (player[this.levelIndex].y - windowHeight / 2);
-        for(let i = 0; i < this.nowFrameData.length; i++){
-            let tileId = this.nowFrameData[i]; 
-            if (tileId === 0) { // igonore empty tiles
-                continue;  
-            }
-            let coord1 = getTilePosition(tileId);
-            let coord2 = getDrawPosition(i, this.levelIndex);
-            image(
-                assets.icon,  
-                coord2.x-offsetX, coord2.y-offsetY,  
-                tileSize, tileSize,    
-                coord1.x, coord1.y,  
-                tileSize, tileSize      
-            );
-        }
+        image(
+            assets.icon,  
+            this.x-offsetX, this.y-offsetY,   
+            tileSize, tileSize,    
+            coordinate.x, coordinate.y,  
+            tileSize, tileSize      
+        );
+        
     }
 
 
