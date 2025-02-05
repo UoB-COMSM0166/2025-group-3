12424434008023bedd06e8860b7511ef1c3acf14
pluvo070 <30020422+pluvo070@users.noreply.github.com>
2025-02-05 00:30:59 +0000

    add climb and ice class

--- docs/game/classes/ice.js
+++ docs/game/classes/ice.js
@@ -1,10 +1,31 @@
 //冰面和冰墙
-
+// 在attach类中添加检测方法:冰面碰撞时不可跳跃
 class Ice {
-    //构造函数
 
-    //update
+    constructor(data, levelIndex) {
+        this.data = data;
+        this.levelIndex = levelIndex;
+    }
+    
+    show() {
+        let offsetX = (player[this.levelIndex].x - windowWidth / 2);
+        let offsetY = (player[this.levelIndex].y - windowHeight / 2);
+        for(let i=0; i<this.data.length; i++){
+            let tileId = this.data[i]; 
+            if (tileId === 0) {  // igonore empty tiles
+                continue;  
+            }
+            let coord1 = getTilePosition(tileId);
+            let coord2 = getDrawPosition(i, this.levelIndex);
+            image(
+                assets.icon,  
+                coord2.x-offsetX, coord2.y-offsetY,  
+                tileSize, tileSize,    
+                coord1.x, coord1.y,  
+                tileSize, tileSize      
+            );
+        }
+    }
 
-    //show
 
 }
