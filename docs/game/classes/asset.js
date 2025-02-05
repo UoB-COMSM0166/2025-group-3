1a4a842789dc4be40049da2025cac7c32f1fe14c
pluvo070 <1478706539@qq.com>
2025-02-05 00:58:45 +0000

    add parse functions

--- docs/game/classes/asset.js
+++ docs/game/classes/asset.js
@@ -1,7 +1,9 @@
 // 加载和存储所有素材对象
+// load and store all assets in an object 
 
 let assets = {};
 
 function preload() {
-
+    assets.icon = loadImage("asset/spritesheet.png");
+    assets.testcat = loadImage("asset/testcat.png");//仅作测试,后续删除
 }
