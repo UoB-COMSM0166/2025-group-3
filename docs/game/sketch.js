9c174a2deca52bdd25789ebc0a403ec02e0fa5ae
pluvo070 <1478706539@qq.com>
2025-02-05 02:54:42 +0000

    add key monitoring

--- docs/game/sketch.js
+++ docs/game/sketch.js
@@ -39,9 +39,9 @@ function drawLevelSelectScreen() {
   strokeWeight(3);
   stroke(255, 255, 255);
   text("Use LEFT/RIGHT to chose\nPress SPACE to begin", width/2, 100);
-  for (let i = 0; i < levels.length; i++) {
+  for (let i = 0; i < levelList.length; i++) {
     let size = 60;
-    let x = width/2 - (levels.length*size)/2 + i*size; 
+    let x = width/2 - (levelList.length*size)/2 + i*size; 
     let y = height/2;
     if (i === selectedLevel) {
       fill(255, 255, 0); // color when selected
@@ -53,7 +53,7 @@ function drawLevelSelectScreen() {
     textSize(26);
     strokeWeight(3);
     stroke(255, 255, 255);
-    text(levels[i], x + size/2, y + size/2); 
+    text(levelList[i], x + size/2, y + size/2); 
   }
 }
 
@@ -92,7 +92,39 @@ function drawGameOverScreen() {}
 function drawLevelCompleteScreen() {}
 
 
-function keyPressed() {}
+function keyPressed() {
+  if (gameState === "start" && keyCode === ENTER) {
+    gameState = "levelSelect";
+  } else if (gameState === "levelSelect") {
+    if (keyCode === LEFT_ARROW) {
+      selectedLevel = max(0, selectedLevel - 1);
+    } else if (keyCode === RIGHT_ARROW) {
+      selectedLevel = min(levelList.length - 1, selectedLevel + 1);
+    } else if (keyCode === 32) {  // Space
+      gameState = "playing";
+    }
+  } else if (gameState === "gameOver" && key === 'r') {
+    gameState = "start";
+  }
+  else if(gameState ==="playing"){
+    keys[key] = true;// record the key pressed
+    console.log(key + " " + keyCode);//test
+  }
+  else if (gameState === "levelComplete") {
+    // ESC -> select level
+    if (keyCode === ESCAPE) {
+      gameState = "levelSelect";
+    } else {
+      // next level
+      if (selectedLevel < levelList.length - 1) {
+        selectedLevel++;
+        gameState = "playing";
+      } else {
+        gameState = "levelSelect"; // the last level -> select level
+      }
+    }
+  }
+}
 function keyReleased() {}
 
 
