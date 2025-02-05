efb46c4f23ff777f70990c970853ebac5b8b3a81
pluvo070 <30020422+pluvo070@users.noreply.github.com>
2025-02-05 01:52:39 +0000

    fix bugs in parse part

--- docs/game/classes/elevate.js
+++ docs/game/classes/elevate.js
@@ -2,6 +2,21 @@
 
 class EvelatingWall{
     //构造函数
+    constructor(x, y, imgIndex, levelIndex, id, range, towards) {
+        this.x = x;
+        this.y = y;
+        this.levelIndex = levelIndex;
+        this.imgIndex = imgIndex;
+
+        this.id = id; // 属于哪个开关
+        this.towards = towards; // 升降方向
+        this.range = range;// 可以运动的瓦片长度有多少个
+        this.pixelRange = (range-1)*tileSize;
+
+        // 敌人自动巡逻参数
+        this.iniX = x; // 记录起始位置
+        this.facingLeft = true; // 最开始都向左侧移动
+    }
 
     //update
 
