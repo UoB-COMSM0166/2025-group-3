3a0839450019866195b653ed42c72f6dd0d2d006
pluvo070 <30020422+pluvo070@users.noreply.github.com>
2025-02-11 17:50:24 +0000

    Add test environment

---
+++ docs/game/jest.setup.js
@@ -0,0 +1,17 @@
+// 测试脚本初始化
+// 设置所有要用到的全局变量
+
+global.tileSize = 10;
+global.tilemargin = 0;
+
+global.windowHeight = 100; 
+global.windowWidth = 100;
+global.levelHeight = []; 
+global.levelWidth = [];
+
+global.offsetX = 0;  
+global.offsetY = 0;  
+global.frameInterval = 18; 
+
+global.keys = {};
+global.assets = {};
