12fce7eff8f965697a8b05927c61d28387fd516e
Grooveofmimosa <88887018+Grooveofmimosa@users.noreply.github.com>
2025-03-03 21:56:32 +0000

    deploy test finished

--- docs/game/src/core/MapLoader.js
+++ docs/game/src/core/MapLoader.js
@@ -48,8 +48,8 @@ export default class MapLoader {
         // };
 
         const levels = [
-            "../../asset/level1.json",
-            "../../asset/level2-2.json"
+            "/asset/level1.json",
+            "/asset/level2-2.json"
         ];
         
         // 避免 this.levelIndex 影响所有关卡，每个关卡独立加载
