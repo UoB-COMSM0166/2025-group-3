dc6cc2e21c104c2a526c827434c86da8b0940fe3
TYHD <qi24477@bristol.ac.uk>
2025-05-07 16:37:28 +0100

    new map

--- docs/game/src/graphics/GameView.js
+++ docs/game/src/graphics/GameView.js
@@ -343,6 +343,10 @@ export default class GameView {
         //console.log("keysESC: " + this.gameModel.keysESC);
 
         // 按esc加载游戏说明页面, 按任意键关闭, 再次按esc退出到选关页面
+        if(this.levelIndex <=1 ){
+            this.gameModel.showHelp = true;
+        }
+        
         if(this.gameModel.showHelp){
             this.showhelpscreen();
         }
--- docs/game/src/graphics/SpineLayer.js
+++ docs/game/src/graphics/SpineLayer.js
@@ -121,7 +121,7 @@ function load(name, scale) {
         // Create a SkeletonJson instance for parsing the .json file.
         let skeletonJson = new spine.SkeletonJson(atlasLoader);
         // Set the scale to apply during parsing, parse the file, and create a new skeleton.
-        skeletonJson.scale = 0.4;
+        skeletonJson.scale = 0.3;
         let skeletonData = skeletonJson.readSkeletonData(
             assetManager.require(skeletonFile)
         );
@@ -137,7 +137,7 @@ function load(name, scale) {
         atlas1 = assetManager1.require(atlasFile1);
         atlasLoader1 = new spine.AtlasAttachmentLoader(atlas1);
         let skeletonJson1 = new spine.SkeletonJson(atlasLoader1);
-        skeletonJson1.scale = 0.4;
+        skeletonJson1.scale = 0.3;
         let skeletonData1 = skeletonJson1.readSkeletonData(
             assetManager1.require(skeletonFile1)
         );
@@ -150,7 +150,7 @@ function load(name, scale) {
         atlas2 = assetManager2.require(atlasFile2);
         atlasLoader2 = new spine.AtlasAttachmentLoader(atlas2);
         let skeletonJson2 = new spine.SkeletonJson(atlasLoader2);
-        skeletonJson2.scale = 0.4;
+        skeletonJson2.scale = 0.3;
         let skeletonData2 = skeletonJson2.readSkeletonData(
             assetManager2.require(skeletonFile2)
         );
@@ -163,7 +163,7 @@ function load(name, scale) {
         atlas3 = assetManager3.require(atlasFile3);
         atlasLoader3 = new spine.AtlasAttachmentLoader(atlas3);
         let skeletonJson3 = new spine.SkeletonJson(atlasLoader3);
-        skeletonJson3.scale = 0.4;
+        skeletonJson3.scale = 0.3;
         let skeletonData3 = skeletonJson3.readSkeletonData(
             assetManager3.require(skeletonFile3)
         );
@@ -175,7 +175,7 @@ function load(name, scale) {
         atlas4 = assetManager3.require(atlasFile3);
         atlasLoader4 = new spine.AtlasAttachmentLoader(atlas4);
         let skeletonJson4 = new spine.SkeletonJson(atlasLoader4);
-        skeletonJson4.scale = 0.4;
+        skeletonJson4.scale = 0.3;
         let skeletonData4 = skeletonJson4.readSkeletonData(
             assetManager3.require(skeletonFile3)
         );
